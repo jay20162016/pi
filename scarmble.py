bccf5a5c001187d68180b097865bd7bc742106c1
jay20162016 <jayjay.jay20162016@gmail.com>
2016-07-16 10:46:05 -0400

    Update scarmble.py

--- scarmble.py
+++ scarmble.py
@@ -1,2 +1,7 @@
+num1 = raw_input('what is number 1')
+num1 = raw_input('what is number 2')
+num1 = raw_input('what is number 3')
+num1 = raw_input('what is number 4')
+
 def shift(direction,number):
   
