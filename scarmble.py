b2520fe6e79202a90343748e165c073063070069
jay20162016 <jayjay.jay20162016@gmail.com>
2016-07-16 14:59:07 -0400

    Update scarmble.py

--- scarmble.py
+++ scarmble.py
@@ -1,7 +1,15 @@
-num1 = raw_input('what is number 1')
-num1 = raw_input('what is number 2')
-num1 = raw_input('what is number 3')
-num1 = raw_input('what is number 4')
+oldnum1 = num1  = raw_input('what is number 1')
+oldnum2 = num2  = raw_input('what is number 2')
+oldnum3 = num3  = raw_input('what is number 3')
+oldnum4 = num4 = raw_input('what is number 4')
+numbers = [num1,num2,num3,num4]
 
-def shift(direction,number):
+def update():
+  oldnum1 = num1
+  oldnum2 = num2
+  oldnum3 = num3
+  oldnum4 = num4
+def swich(direction,number2,number1):
+  num[ list(range(1,numbers+1)[number1] ] = num[ list(range(1,numbers+1)[number2] ]
+  update()
   
